2207f8d66fac50ed64b3818ffd1c42ab7dba5d70
aper77 <bryanmayis95@gmail.com>
2019-09-27 13:18:40 +0300

    LessonTwoExersizeTwo

---
+++ src/LessonTwoExersizeTwo.java
@@ -0,0 +1,10 @@
+public class LessonTwoExersizeTwo {
+    public static void main(String[] args) {
+        int a = 5;
+        int i = 0;
+        for (; i < a; ) {
+            int sum = i + (i++);
+            System.out.println(sum);
+        }
+    }
+}
