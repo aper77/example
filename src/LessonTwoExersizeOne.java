bd90ec9171faed95889a7d79ed602df57dfb3fe8
aper77 <bryanmayis95@gmail.com>
2019-09-27 10:57:14 +0300

    LessonTwoExersizeOnee

---
+++ src/LessonTwoExersizeOne.java
@@ -0,0 +1,9 @@
+public class LessonTwoExersizeOne {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            int cost = 150;
+            int allCost = i * cost;
+            System.out.println(i + " kg" + " arje " + allCost + "  dram");
+        }
+    }
+}
