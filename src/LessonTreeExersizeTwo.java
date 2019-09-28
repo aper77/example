915897061a3e10f442aa32158f38514e69e94dbf
aper77 <bryanmayis95@gmail.com>
2019-09-28 15:54:29 +0300

    LessonTreeExersizeTwo

---
+++ src/LessonTreeExersizeTwo.java
@@ -0,0 +1,27 @@
+public class LessonTreeExersizeTwo{
+    public static void main(String[] args) {
+        MyCar myObj = new MyCar();
+        myObj.engine = 5;
+        myObj.speed = 280;
+        System.out.print(myObj.engine);
+    }
+
+   public static class MyCar {
+         int engine;
+         String tayp;
+         int wheels;
+         int speed;
+         int stop;
+         String signals;
+
+         public void carEngine(porshine,moshnc,ves){
+        System.out.print("Car can go");
+       }
+
+       public void carWheels(tyap,diametr,firmu){
+             
+       }
+
+
+    }
+}
