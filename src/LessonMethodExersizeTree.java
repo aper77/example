00c5d5890851dabb00cd5ba6abadad21d29e4e50
aper77 <bryanmayis95@gmail.com>
2019-10-20 18:31:09 +0300

    LessonMethodExersizeTree

---
+++ src/LessonMethodExersizeTree.java
@@ -0,0 +1,11 @@
+public class LessonMethodExersizeTree {
+    public static void main(String[] args) {
+        int size =size("Aramayis") ;
+        System.out.println(size);
+
+    }
+    public static int size(String x){
+        int a = x.length();
+        return a;
+    }
+}
