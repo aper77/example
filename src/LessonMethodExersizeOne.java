e123a3b48f34122f7a62dbb0335062ac350b5897
aper77 <bryanmayis95@gmail.com>
2019-10-20 18:04:53 +0300

    LessonMethodExersizeOne

---
+++ src/LessonMethodExersizeOne.java
@@ -0,0 +1,10 @@
+public class LessonMethodExersizeOne {
+    public static void main(String[] args) {
+       int sum1 = sum(10,0);
+        System.out.println(sum1);
+    }
+    public static int sum(int x,int y){
+       int z = x + y;
+       return z;
+    }
+}
