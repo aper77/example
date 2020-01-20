fec4bdbaafe6f12fbadb980bd662a2f59c84dc53
aper77 <bryanmayis95@gmail.com>
2020-01-20 14:55:16 +0300

    JavaBuilder

--- src/ProgramRaner.java
+++
@@ -1,9 +0,0 @@
-public class ProgramRaner {
-    public static void main(String[] args) {
-        JavaSingltone.getJavaSingltone().addLogInfo("First.log");
-        JavaSingltone.getJavaSingltone().addLogInfo("Secand.log");
-        JavaSingltone.getJavaSingltone().addLogInfo("four.log");
-
-        JavaSingltone.getJavaSingltone().showLogFile();
-    }
-}
