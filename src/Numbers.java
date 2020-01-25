45d2804f8a5a68c7909bfb0c47504597dc2590f1
aper77 <bryanmayis95@gmail.com>
2020-01-25 18:20:18 +0300

    Numbers

---
+++ src/Numbers.java
@@ -0,0 +1,16 @@
+public class Numbers {
+    public static void main(String[] args) {
+
+        /** ete arajin tvi ev erkrord tvi tarberutyun@ bacasakan tiv e cuyc tal**/
+
+        int x = 30;
+        int y = 29;
+        int z = x-y;
+        if (x-y>=0){
+            System.out.println(z);
+        }else{
+            System.out.println("z > 0");
+       }
+
+    }
+}
